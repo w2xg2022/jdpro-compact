2677c86749f4d40c32353a5d3d618c4dd8d95a50
woody <woody@local>
2026-06-27 19:52:48 +0800

    fix: use simplified Chinese characters consistently in reminder message

--- jd_remind_cookie.sh
+++ jd_remind_cookie.sh
@@ -2,5 +2,5 @@
 # 提醒手动更新 JD_COOKIE，不做任何登录/抓取动作
 curl -s "${NTFY_URL:-https://ntfy.sh}/${NTFY_TOPIC}" \
   -H "Title: 提醒更新京东Cookie" \
-  -d "现在是早上6:55，记得用手机APP掃碼更新一下JD_COOKIE～"
+  -d "现在是早上6:55，记得用手机APP扫码更新一下JD_COOKIE～"
 echo "已发送cookie更新提醒"
